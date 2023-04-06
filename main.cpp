5a47bc067aa48b4af1bcbbd84a3d6e9e62649231
GoldilocksKang <ceo5584@gmail.com>
2023-04-06 09:17:43 +0900

    0406 초기화

--- main.cpp
+++ main.cpp
@@ -3,79 +3,43 @@
 
 using namespace std;
 
+class Character
+{
+	bool Moveable = true;
+};
+
+//Character
+class Player : public Character
+{
+	bool CanAttackMonster = true;
+	int HP = 100;
+	int MP = 100;
+};
+
+class Monster : public Character
+{
+	bool CanAttackPlayer = true;
+};
+
+//Monster
+class Slime : public Monster
+{
+
+};
+
+class Goblin : public Monster
+{
+
+};
+
+class WildBoar : public Monster
+{
+
+};
+
 int main()
 {
-	int Map[10][10] = {
-		{1,1,1,1,1,1,1,1,1,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1}
-	};
-
-	int PlayerX = 1;
-	int PlayerY = 1;
-	bool bIsRunning = true;
-
-	while (true) //GameLoop
-	{
-
-		//Frame 한 장을 그린다
-		
-		//어떤 엔진이든 하는 3가지
-		//Input();
-		char Key;
-		Key = _getch();
-		//Tick();
-		if (Key == 'd')
-		{
-			PlayerX++;
-		}
-		else if (Key == 'a')
-		{
-			PlayerX--;
-		}
-		else if (Key == 's')
-		{
-			PlayerY++;
-		}
-		else if (Key == 'w')
-		{
-			PlayerY--;
-		}
-		else if (Key == 'q')
-		{
-			bIsRunning = false;
-		}
-
-		//Render();
-		system("cls");  // 지우기, 실제로는 사용X, cls 대신 다른걸 쓸 수 있어서 해킹당함
-
-		for (int Y = 0; Y < 10; ++Y)
-		{
-			for (int X = 0; X < 10; ++X)
-			{
-				if (PlayerX == X && PlayerY == Y)
-				{
-					cout << "P ";
-				}
-				else if (Map[Y][X] == 1)
-				{
-					cout << "# ";
-				}
-				else if (Map[Y][X] == 0)
-				{
-					cout << "  ";
-				}
-			}
-			cout << endl;
-		}
-	}
+	
 	
 	return 0;
 }
